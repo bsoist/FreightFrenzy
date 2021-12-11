99e6cb6be2be24de2f9ad3a6d68cd6a09ce746f2
milesthomas <ths22115@towerhill.org>
2021-12-11 12:36:06 -0500

    on button press brakes added to main bot teleop file (basicOpMode)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -33,6 +33,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -46,6 +48,9 @@ public class BasicOpMode_Linear extends LinearOpMode {
     private DcMotor bRight = null;
     private DcMotor fLeft = null;
     private DcMotor fRight = null;
+    private DcMotor elbow    = null;
+    private DcMotor shoulder = null;
+    private Servo   claw     = null;
 
     /**
      * Manages toggling an action.
@@ -62,13 +67,26 @@ public class BasicOpMode_Linear extends LinearOpMode {
         DcMotorEx bRight = hardwareMap.get(DcMotorEx.class, "backRight");
         DcMotorEx fLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
         DcMotorEx fRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        elbow    = hardwareMap.get(DcMotor.class,   "elbowArmMotor");
+        shoulder = hardwareMap.get(DcMotor.class,"shoulderArmMotor");
+        claw     = hardwareMap.get(Servo.  class,            "claw");
+
         bLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shoulder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-         bRight.setDirection(DcMotor.Direction.REVERSE);
-         fRight.setDirection(DcMotor.Direction.REVERSE);
+        elbow.setDirection(DcMotorSimple.Direction.REVERSE);
+        bRight.setDirection(DcMotor.Direction.REVERSE);
+        fRight.setDirection(DcMotor.Direction.REVERSE);
+
+        double armSpeed = .75;
+        claw.setPosition(-1.0);
+        boolean elbowBrake = false;
+        boolean shoulderBrake = false;
+
 
         waitForStart();
         runtime.reset();
@@ -76,34 +94,71 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            double leftPower;
-            double rightPower;
+//            double leftPower;
+//            double rightPower;
+            int elbowCurrentPosition    =    elbow.getCurrentPosition();
+            int shoulderCurrentPosition = shoulder.getCurrentPosition();
+            int shoulderBrakePosition = 0;
+            int elbowBrakePositon = 0;
 
             double x =  gamepad1.right_stick_x;
             double y =  -gamepad1.right_stick_y;
-//            double speed = Range.clip(gamepad1.left_stick_y, -1.0, 0);
             double motorVelocity = -gamepad1.left_stick_y * 2500;
-
             double leftDirection = y + x;
             double rightDirection = y - x;
-
+            double leftVelocity   = leftDirection * motorVelocity;
+            double rightVelocity   = rightDirection * motorVelocity;
+//            double speed = Range.clip(gamepad1.left_stick_y, -1.0, 0)
 //            leftPower    = Range.clip(leftDirection * speed, -1.0, 1.0);
 //            rightPower   = Range.clip(rightDirection * speed, -1.0, 1.0);
 
-            double leftVelocity   = leftDirection * motorVelocity;
-            double rightVelocity   = rightDirection * motorVelocity;
+            double elbowPower    = Range.clip(gamepad2.left_stick_y , -1.0, 1.0) * armSpeed;
+            double shoulderPower = Range.clip(gamepad2.right_stick_y, -1.0, 1.0) * armSpeed;
 
-
-//             Send calculated power to wheels
 //            bLeft.setPower(leftPower);
 //            bRight.setPower(rightPower);
 //            fLeft.setPower((leftPower));
 //            fRight.setPower(rightPower);
-
             bLeft.setVelocity(leftVelocity);
             fLeft.setVelocity(leftVelocity);
             bRight.setVelocity(rightVelocity);
             fRight.setVelocity(rightVelocity);
+
+            //elbow brake conditions
+            if (gamepad2.left_stick_button & !elbowBrake) {
+                elbowBrake = true;
+            }
+            else if (gamepad2.left_stick_button & elbowBrake) {
+                elbowBrake = false;
+            }
+            //shoulder brake conditions
+            if (gamepad2.right_stick_button & !shoulderBrake) {
+                shoulderBrake = true;
+            }
+            else if (gamepad2.right_stick_button & shoulderBrake) {
+                shoulderBrake = false;
+            }
+
+            //the brakes themselves
+            if (elbowBrake) {
+                elbowBrakePositon = elbowCurrentPosition;
+                elbow.setTargetPosition(elbowBrakePositon);
+                elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            else {
+                elbow.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                elbow.setPower(elbowPower);
+            }
+
+            if (shoulderBrake) {
+                shoulderBrakePosition = shoulderCurrentPosition;
+                shoulder.setTargetPosition(shoulderBrakePosition);
+                shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            else {
+                shoulder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                shoulder.setPower(shoulderPower);
+            }
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -113,6 +168,10 @@ public class BasicOpMode_Linear extends LinearOpMode {
                             bLeft.getPower(), bRight.getPower(), fLeft.getPower(),  fRight.getPower());
             telemetry.addData("Theoretical Velocity", "left (%.2f), right (%.2f)", leftVelocity, rightVelocity);
             telemetry.addData("Motor Direction", "left (%.2f), right (%.2f)", leftDirection, rightDirection);
+            telemetry.addData("Claw"          , "Position: (%.2f)"             , claw.getPosition()              );
+            telemetry.addData("Shoulder"      , "Power: (%.2f), Position: (%d), Target Position: (%d), Brake: (%b)", shoulderPower, shoulderCurrentPosition, shoulder.getTargetPosition(), shoulderBrake);
+            telemetry.addData("Elbow"         , "Power: (%.2f), Position: (%d), Target Position: (%d), Brake: (%b)", elbowPower, elbowCurrentPosition, elbow.getTargetPosition(), elbowBrake);
+
             telemetry.update();
         }
     }
