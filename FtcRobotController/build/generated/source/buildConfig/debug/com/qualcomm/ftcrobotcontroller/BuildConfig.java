914c873a93462ddea707756a421fd83d5f8151c9
milesthomas <ths22115@towerhill.org>
2022-02-16 19:53:16 -0500

    new intake (NewIntake) > have to fix zero power on intake next limits for elbow?

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-02-12T14:34:42.127-0500";
+  public static final String APP_BUILD_TIME = "2022-02-16T19:31:18.602-0500";
 }
